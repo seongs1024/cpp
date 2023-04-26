8d7c94a4a183d34fb353c65a85e9c9d0f8bc3471
Seongsu Park <seongspa@c2r4s1.42seoul.kr>
2023-04-26 13:24:49 +0900

    ex03

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: seongspa <seongspa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 12:00:55 by seongspa          #+#    #+#             */
-/*   Updated: 2023/04/26 12:17:58 by seongspa         ###   ########.fr       */
+/*   Updated: 2023/04/26 13:22:51 by seongspa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name): _name(name) {}
+HumanB::HumanB(std::string name): _name(name), _weapon(NULL) {}
 
 void HumanB::attack() const
 {
-    std::cout << this->_name << " attacks with their " << this->_weapon.getType() << std::endl;
+    if (this->_weapon)
+        std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
+    else
+        std::cout << this->_name << " cannot attack because they don't have a weapon" << std::endl;
 }
 
-void HumanB::setWeapon(Weapon &weapon)
+void HumanB::setWeapon(Weapon& weapon)
 {
-    this->_weapon = weapon;
+    this->_weapon = &weapon;
 }
--- ex03/HumanB.hpp
+++ ex03/HumanB.hpp
@@ -6,7 +6,7 @@
 /*   By: seongspa <seongspa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 12:00:54 by seongspa          #+#    #+#             */
-/*   Updated: 2023/04/26 12:19:43 by seongspa         ###   ########.fr       */
+/*   Updated: 2023/04/26 13:01:47 by seongspa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ class HumanB
 {
 private:
     std::string _name;
-    Weapon& _weapon;
+    Weapon* _weapon;
 public:
     HumanB(std::string name);
     void attack() const;
