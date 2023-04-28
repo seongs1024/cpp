39f46ee4ecf676fc60a6e45ce00ec37b3a791ff5
astu <56729421+seongs1024@users.noreply.github.com>
2023-04-28 14:41:47 +0900

    ex05

---
+++ ex05/Harl.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+#include "Harl.hpp"
+
+void Harl::complain(std::string level)
+{
+	void (Harl::* const FUNC_TABLE[])(void) = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
+	const std::string LEVELS[] = { "debug", "info", "warning", "error" };
+
+	for (size_t i = 0; i < level.length(); i++)
+	{
+		level[i] = tolower(level[i]);
+	}
+	for (size_t i = 0; i < 4; i++)
+	{
+		if (LEVELS[i] == level)
+		{
+			(this->*FUNC_TABLE[i])();
+			break;
+		}
+	}
+}
+
+void Harl::debug(void)
+{
+	std::cout << "DEBUG: debug" << std::endl;
+}
+
+void Harl::info(void)
+{
+	std::cout << "INFO: info" << std::endl;
+}
+
+void Harl::warning(void)
+{
+	std::cout << "WARNING: warning" << std::endl;
+}
+
+void Harl::error(void)
+{
+	std::cout << "ERROR: error" << std::endl;
+}
---
+++ ex05/main.cpp
@@ -0,0 +1,11 @@
+#include "Harl.hpp"
+
+int main()
+{
+	Harl harl;
+	harl.complain("HI");
+	harl.complain("debug");
+	harl.complain("INFO");
+	harl.complain("warNing");
+	harl.complain("Error");
+}
