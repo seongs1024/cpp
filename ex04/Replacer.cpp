e7c0bb3ca56b2adb671e4e9af2d65c69a561996f
astu <56729421+seongs1024@users.noreply.github.com>
2023-04-28 13:14:21 +0900

    fix linux competability

--- ex04/Replacer.cpp
+++ ex04/Replacer.cpp
@@ -6,11 +6,11 @@
 
 Replacer::Replacer(std::string infile): _infile(infile)
 {
-    std::ifstream ifs(infile);
+    std::ifstream ifs(infile.c_str());
     if (ifs.fail())
         throw std::runtime_error("No such file: " + infile);
     std::string outfile(infile + ".replace"); 
-    std::ifstream temp_fs(outfile);
+    std::ifstream temp_fs(outfile.c_str());
     if (temp_fs.good())
     {
         std::cout << "\"" << outfile << "\" exits. Are you sure to replace it? [Y/n]" << std::endl;
@@ -26,10 +26,10 @@ void Replacer::replace(std::string from, std::string to)
 {
     if (from.empty())
         throw std::runtime_error("Empty string");
-    std::ifstream ifs(this->_infile);
+    std::ifstream ifs(this->_infile.c_str());
     std::ostringstream oss; oss << ifs.rdbuf();
     std::string origin = oss.str();
-    std::ofstream ofs(this->_outfile);
+    std::ofstream ofs(this->_outfile.c_str());
     size_t next = origin.find(from);
     ofs << origin.substr(0, next);
     while (next != std::string::npos)    
