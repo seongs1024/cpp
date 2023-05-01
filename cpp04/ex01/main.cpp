24a1ad0eb141cebe85aa0daac28a7def98931aad
Seongsu Park <seongspa@c2r4s1.42seoul.kr>
2023-05-01 16:38:52 +0900

    copy assign

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: seongspa <seongspa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 13:59:04 by seongspa          #+#    #+#             */
-/*   Updated: 2023/05/01 16:25:32 by seongspa         ###   ########.fr       */
+/*   Updated: 2023/05/01 16:38:07 by seongspa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,6 @@ int main()
 {
     Animal* j = new Dog();
     Animal* i = new Cat();
- 
-    *i = Dog();
     
     delete i;
     delete j;
@@ -43,17 +41,8 @@ int main()
     }
 
     std::cout << "+++++++" << std::endl;
-    Animal * animals2 = new Animal[2 * ANIMALS_HALF];
-    for (size_t i = 0; i < ANIMALS_HALF; i++) {
-        animals2[i] = Dog();
-        animals2[ANIMALS_HALF + i] = Cat();
-    }
-    for (size_t i = 0; i < 2 * ANIMALS_HALF; i++) {
-        std::cout << animals2[i].getType() << std::endl;
-        animals2[i].makeSound();
-    }
-    delete[] animals2;
-
-    system("leaks animal");
+    Dog coco;
+    coco = Dog();
+    
     return 0;
 }
