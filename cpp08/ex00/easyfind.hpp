7c3c0ca6fc61ae78d92de6e2deef8ef81428bacf
Seongsu Park <seongspa@c5r2s7.42seoul.kr>
2023-06-15 16:40:21 +0900

    cpp08 ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -1,12 +1,8 @@
 #pragma once
 #include <algorithm>
-#include <exception>
 
 template<typename T>
-bool easyfind(T & a, int b)
+typename T::const_iterator easyfind(T & a, int b)
 {
-    if (std::find(a.begin(), a.end(), b) != a.end())
-        return true;
-    else
-        throw std::runtime_error("No occurrence!");
+    return std::find(a.begin(), a.end(), b);
 }
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -9,38 +9,17 @@ int main( void ) {
     std::vector<int> vec(arr, arr + 10);
     std::deque<int> deq(arr, arr + 10);
     std::list<int> list(arr, arr + 10);
-    try
-    {
-        if (easyfind(vec, 7))
-            std::cout << "7 is found" << std::endl;
-        if (easyfind(vec, 42))
-            std::cout << "42 is found" << std::endl;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        if (easyfind(deq, 7))
-            std::cout << "7 is found" << std::endl;
-        if (easyfind(deq, 42))
-            std::cout << "42 is found" << std::endl;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        if (easyfind(list, 7))
-            std::cout << "7 is found" << std::endl;
-        if (easyfind(list, 42))
-            std::cout << "42 is found" << std::endl;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+    if (easyfind(vec, 7) != vec.end())
+        std::cout << "7 is found" << std::endl;
+    if (easyfind(vec, 42) != vec.end())
+        std::cout << "42 is found" << std::endl;
+    if (easyfind(deq, 7) != deq.end())
+        std::cout << "7 is found" << std::endl;
+    if (easyfind(deq, 42) != deq.end())
+        std::cout << "42 is found" << std::endl;
+    if (easyfind(list, 7) != list.end())
+        std::cout << "7 is found" << std::endl;
+    if (easyfind(list, 42) != list.end())
+        std::cout << "42 is found" << std::endl;
     return 0;
 }
